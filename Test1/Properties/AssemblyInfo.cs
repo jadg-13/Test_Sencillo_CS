6c1013ac1085f22db0cf7797cd04998909dd0a15
jadg13 <86695996+jadg-13@users.noreply.github.com>
2024-11-19 13:07:30 -0600

    Agregar archivos de proyecto.

---
+++ Test1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Test1")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0e21e76d-8677-4178-8ba4-f0f2e4692187")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Test1/UnitTest1.cs
@@ -0,0 +1,61 @@
+﻿using Calculadora.Modelos;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Test1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void TestSuma()
+        {
+            double num1 = 3, num2 = 6;
+            Operacion op = new Operacion();
+            double resultado = op.Sumar(num1, num2);
+            Assert.AreEqual(9, resultado);
+        }
+
+        [TestMethod]
+        public void TestResta()
+        {
+            double num1 = 3, num2 = 6;
+            Operacion op = new Operacion();
+            double resultado = op.Restar(num1, num2);
+            Assert.AreEqual(-3, resultado);
+        }
+
+        [TestMethod]
+        public void TestMultiplicacion()
+        {
+            double num1 = 3, num2 = 6;
+            Operacion op = new Operacion();
+            double resultado = op.Multiplicar(num1, num2);
+            Assert.AreEqual(18, resultado);
+        }
+
+        [TestMethod]
+        public void TestDivision()
+        {
+            double num1 = 3, num2 = 6;
+            Operacion op = new Operacion();
+            double resultado = op.Dividir(num1, num2);
+            Assert.AreEqual(0.5, resultado);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void TestDivisionPorCero()
+        {
+            double num1 = 3, num2 = 0;
+            Operacion op = new Operacion();
+            double resultado = op.Dividir(num1, num2);
+        }
+    }
+
+}
